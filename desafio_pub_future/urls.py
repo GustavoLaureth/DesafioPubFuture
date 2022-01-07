6eabfa6e114a22293caf6289c693c9a4f71eaa74
Gustavo Laureth <gustavolaureth22@gmail.com>
2022-01-07 15:48:05 -0300

    Paginas login, register e dashboard criadas

--- desafio_pub_future/urls.py
+++ desafio_pub_future/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.login, name='login'),
+    path('cadastro/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard')
 ]
--- desafio_pub_future/views.py
+++ desafio_pub_future/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'pages/index.html')
+def login(request):
+    return render(request, 'pages/login.html')
+
+def register(request):
+    return render(request, 'pages/register.html')
+
+def dashboard(request):
+    return render(request, 'pages/dashboard.html')
